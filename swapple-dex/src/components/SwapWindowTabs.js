7750c1e93965e301de68328994559cbfe6912aff
unknown <dszemik1@gmail.com>
2022-04-03 17:47:42 +0200

    added input forms, buttons

--- swapple-dex/src/components/SwapWindowTabs.js
+++ swapple-dex/src/components/SwapWindowTabs.js
@@ -1,5 +1,8 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './react-tabs.css';
+import settingsGear from '../img/SettingsGear.png';
+import arrowIcon from '../img/arrow.svg';
+import connectWalletIcon from '../img/connect-wallet-icon.png';
 
 const SwapWindowTabs = () => {
     const liquidityClicked = () => {
@@ -14,10 +17,24 @@ const SwapWindowTabs = () => {
         
             <TabPanel>
                 <div className="Rectangle-8">
-                    <div className="SwapCoinsText">Swap Coins</div>
+                    <img src={settingsGear} className="settings-gear" alt="settings"/>
+                    <span className="SwapCoinsText">Swap Coins</span>
+                    <div className="from-text">From</div>
+                    <div className="from-box">
+                        <input type="text" autocomplete="off" className="from-input" id="fname" name="fname" placeholder="Enter amount"></input>
+                    </div>
+                    <div className="swap-around-button">
+                        <img src={arrowIcon} className="arrow-icon" alt="swap"/>
+                    </div>
+                    <div className="to-text">To</div>
+                    <div className="to-box">
+                        <input type="text" autocomplete="off" className="to-input" id="fname" name="fname" placeholder="Enter amount"></input>
+                        <span className="dollar-amount">$</span>
+                    </div>
 
                     <div className="Rectangle-12">
                         <div className="ConnectWalletText">CONNECT WALLET</div>
+                        <img src={connectWalletIcon} className="connect-wallet-icon" alt="settings"/>
                     </div>   
                 </div>
             </TabPanel>
